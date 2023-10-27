034959a6b16d7b35cc98a11bfa77879bcbc942b1
Luis Olavarria <kukiluis89@gmail.com>
2023-10-26 18:45:58 -0600

    first test

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [ ['html', { open: 'never' }] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,10 +33,10 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
     // {
     //   name: 'firefox',
@@ -63,10 +63,10 @@ module.exports = defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/searchFlight.spec.js
+++ tests/searchFlight.spec.js
@@ -13,9 +13,16 @@ test('search fligh ny to berlin', async ({ page, context }) => {
   await page.locator('[data-test-id="passengers-field"]').click();
   await page.locator('[data-test-id="passengers-adults-field"] a').nth(1).click();
   
+  //wait for new page to open after submit search
   const page1Promise = page.waitForEvent('popup');
   await page.locator('[data-test-id="form-submit"]').click();
   const page1 = await page1Promise;
-  await expect(page1).toHaveTitle(/Aviasales/);
-  
+
+  //assertions
+  await expect(page1).toHaveTitle(/NYC/);
+  await expect(page1.locator('#origin')).toHaveValue('John F. Kennedy International Airport');
+  await expect(page1.locator('#destination')).toHaveValue('Berlin');
+  await expect(page1.locator('[data-test-id="departure-date-input"]')).toHaveValue('Mon, October 30');
+  await expect(page1.locator('[data-test-id="return-date-input"]')).toHaveValue('');
+  await expect(page1.locator('.additional-fields div:nth-of-type(2)')).toHaveText('2 passengers');
 });
