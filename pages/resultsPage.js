780298f4f953bfa8d5f7a233fa36727ab3fb12c3
Luis Olavarria <kukiluis89@gmail.com>
2023-10-27 01:45:24 -0600

    separate results page

---
+++ pages/resultsPage.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+
+exports.ResultsPage = class resultsPage{
+    constructor(page){
+        this.page = page
+
+        this.origin_input = page.locator('#origin');
+        this.destination_input = page.locator('#destination'); 
+        this.departureDate_input = page.locator('[data-test-id="departure-date-input"]');
+        this.noReturn_Btn = page.locator('[data-test-id="no-return-ticket"]');
+        this.returnDate_input = page.locator('[data-test-id="return-date-input"]');
+        this.passengersNum_field = page.locator('.additional-fields div:nth-of-type(2)');
+    }
+
+    async validateResults(origin, airportOrigin, destination, departureDate, passengersNum){
+        await expect(this.origin_input).toHaveValue(airportOrigin);
+        await expect(this.destination_input).toHaveValue(destination);
+        let options = { weekday: 'short', month: 'long', day: 'numeric' };
+        let depDate  = new Date(departureDate);
+        let formattedDate = depDate.toLocaleDateString("en-US", options);
+        await expect(this.departureDate_input).toHaveValue(formattedDate);
+        await expect(this.returnDate_input).toHaveValue('');
+        await expect(this.passengersNum_field).toHaveText(passengersNum + ' passengers');
+    }
+}
--- tests/searchFlight.spec.js
+++ tests/searchFlight.spec.js
@@ -1,17 +1,19 @@
 const { test, expect } = require('@playwright/test');
 import { SearchPage } from '../pages/searchPage'
+import { ResultsPage } from '../pages/resultsPage'
 
 test('Search flight', async ({ page, context }) => {
 
+  //Search page actions
   const Search = new SearchPage(page);
- 
   await Search.gotoSite()
   //fillSearchFields method expects city of Origin , airport of Origin and city of destination
   let originCity,originAirport ,destination, departureDate,passengersNum
   let array = [originCity = 'New York', originAirport = 'John F. Kennedy International Airport', destination = 'Berlin', departureDate = "10-30-2023", passengersNum = 2]
   await Search.fillSearchFields(...array)
-  const newTab = await Search.submitSearch()
-  const Results = new SearchPage(newTab);
+  const newTab = await Search.submitSearch();
+  //Results page validations
+  const Results = new ResultsPage(newTab);
   await Results.validateResults(...array)
   
 });
